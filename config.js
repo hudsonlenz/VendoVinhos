f1b525545d9ba390f785c7a38960ea0686a561c7
hudsonlenz <hudson.lenz123@gmail.com>
2026-07-13 18:45:34 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // A "anon public key" é segura para ficar exposta no front-end,
 // pois o acesso real é controlado pelas políticas RLS no banco.
 // ============================================================
-const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SEU_PROJETO";
-const SUPABASE_ANON_KEY = "COLE_AQUI_A_ANON_KEY";
+const SUPABASE_URL = "https://oqanktrufmmpxxrelgqn.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xYW5rdHJ1Zm1tcHh4cmVsZ3FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5NzAzNzgsImV4cCI6MjA5OTU0NjM3OH0.9PC_EiOCC4bJ0qWho2pbRQOoyfCYMlR61G5DOrbP3R0";
